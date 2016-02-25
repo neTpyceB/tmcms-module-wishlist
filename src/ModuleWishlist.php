bfd9ac87dfbfc519cab037f24ed2dd695acf2607
alpha <alpha@devp.lv>
2016-02-25 11:38:27 +0200

    update wishlist module

--- src/ModuleWishlist.php
+++ src/ModuleWishlist.php
@@ -5,6 +5,8 @@ namespace TMCms\Modules\Wishlist;
 use TMCms\Modules\IModule;
 use TMCms\Modules\Wishlist\Entity\WishlistRelationEntity;
 use TMCms\Modules\Wishlist\Entity\WishlistRelationEntityRepository;
+use TMCms\Orm\Entity;
+use TMCms\Strings\Converter;
 use TMCms\Traits\singletonInstanceTrait;
 
 defined('INC') or exit;
@@ -18,19 +20,18 @@ class ModuleWishlist implements IModule
 	);
 
 	/**
-	 * @param string $item_type e.g. "book"
-	 * @param int $item_id e.g. favored $book_id
+     * @param Entity $item
 	 * @param int $client_id e.g. who favors $client_id
 	 * @return int $id of created entry
 	 */
-	public static function addWish($item_type, $item_id, $client_id) {
-		$wish = self::getWish($item_type, $item_id, $client_id);
+	public static function addWish(Entity $item, $client_id) {
+		$wish = self::getWish($item, $client_id);
 
 		if (!$wish) {
 			$wish = new WishlistRelationEntity();
 			$wish->loadDataFromArray([
-				'item_type' => $item_type,
-				'item_id' => $item_id,
+				'item_type' => Converter::classWithNamespaceToUnqualifiedShort($item),
+				'item_id' => $item->getId(),
 				'client_id' => $client_id,
 			]);
 			$wish->save();
@@ -40,41 +41,39 @@ class ModuleWishlist implements IModule
 	}
 
 	/**
-	 * @param string $item_type
-	 * @param int $item_id
+     * @param Entity $item
 	 * @param int $client_id
 	 */
-	public static function deleteWish($item_type, $item_id, $client_id) {
+	public static function deleteWish(Entity $item, $client_id) {
 		$wishes = new WishlistRelationEntityRepository();
-		$wishes->setWhereItemType($item_type);
-		$wishes->setWhereItemId($item_id);
+		$wishes->setWhereItemType(Converter::classWithNamespaceToUnqualifiedShort($item));
+		$wishes->setWhereItemId($item->getId());
 		$wishes->setWhereClientId($client_id);
 
 		$wishes->deleteObjectCollection();
 	}
 
-	/**
-	 * @param string $item_type
-	 * @param int $item_id
-	 * @param int $client_id
-	 * @return bool
-	 */
-	public static function getWish($item_type, $item_id, $client_id) {
+    /**
+     * @param Entity $item
+     * @param int $client_id
+     * @return bool
+     */
+	public static function getWish(Entity $item, $client_id) {
 		return WishlistRelationEntityRepository::findOneEntityByCriteria([
-				'item_type' => $item_type,
-				'item_id' => $item_id,
+				'item_type' => Converter::classWithNamespaceToUnqualifiedShort($item),
+				'item_id' => $item->getId(),
 				'client_id' => $client_id,
 		]);
 	}
 
-	/**
-	 * @param string $item_type
-	 * @param int $client_id
-	 * @return array
-	 */
-	public static function getWishList($item_type, $client_id) {
+    /**
+     * @param Entity $item
+     * @param int $client_id
+     * @return array
+     */
+	public static function getWishList(Entity $item, $client_id) {
 		$wishes = new WishlistRelationEntityRepository();
-		$wishes->setWhereItemType($item_type);
+		$wishes->setWhereItemType(Converter::classWithNamespaceToUnqualifiedShort($item));
 		$wishes->setWhereClientId($client_id);
 
 		return $wishes->getPairs(['item_id']);
